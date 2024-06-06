89da3f8ef5f53d110353f7232173170bf673029b
murieru <murielhediger@gmail.com>
2024-06-06 16:17:04 +0200

    Category.js component useless useState removed

--- src/comp/Category.js
+++ src/comp/Category.js
@@ -1,11 +1,7 @@
-import '../styles/Category.css'
-import { useState } from 'react';
-import {projectList} from '../data/projectList.js'
+import '../styles/Category.css';
+import { projectList } from '../data/projectList.js';
 
-
-function Category({currentCategory, setCategory}){
-
-
+function Category({ currentCategory, setCategory }) {
     const categoriesList = projectList.reduce((acc, project) => {
         project.category.forEach(category => {
             if (!acc.includes(category)) {
@@ -15,60 +11,32 @@ function Category({currentCategory, setCategory}){
         return acc;
     }, []);
 
-    
-    return(
-
+    return (
         <div id='category'>
             <ul>
-        {categoriesList.map((category) => (
-
-      
-        <li key={category}>
-                  
-                <input id={category} type='checkbox' value={category} onChange={(e) => { 
-
-                    const checkedCategory = e.target.value;
-                    if(e.target.checked){
-                    setCategory([...currentCategory, checkedCategory]) 
-                    }
-                    else{
-                    setCategory(currentCategory.filter(item => item !== checkedCategory));
-                    }
-                    
-                    }
-                } />
-                <label htmlFor={category}>
-            {category}
-            </label>
-        </li>
-        
-
-        ))}
+                {categoriesList.map((category) => (
+                    <li key={category}>
+                        <input 
+                            id={category} 
+                            type='checkbox' 
+                            value={category} 
+                            onChange={(e) => { 
+                                const checkedCategory = e.target.value;
+                                if (e.target.checked) {
+                                    setCategory([...currentCategory, checkedCategory]);
+                                } else {
+                                    setCategory(currentCategory.filter(item => item !== checkedCategory));
+                                }
+                            }} 
+                        />
+                        <label htmlFor={category}>
+                            {category}
+                        </label>
+                    </li>
+                ))}
             </ul>
-
-            
-
         </div>
-
-    )
-
+    );
 }
 
-export default Category
-
-/* OLD return (working)
-
-        <div id='category'>
-            <ul>
-        {categoriesList.map((category) => (
-
-        <li key={category} onClick={(e) => (setCategory(currentCategory=e.target.innerText))
-        }>
-            {category}
-        </li>
-
-        ))}
-            </ul>
-        </div>
-
-*/
+export default Category;
