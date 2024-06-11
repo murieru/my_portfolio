747251fd507f8d7a992328a1ef95a90e4ca0cb94
murieru <murielhediger@gmail.com>
2024-06-11 10:49:03 +0200

    new concrete projects

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -1,67 +1,76 @@
 import placeholder from "../assets/exemple_01.jpg"
 import placeholder_2 from "../assets/logo.svg"
 
+import SONENSTUBE_01 from "../assets/01_SONENSTUBE_01.jpg"
+import SONENSTUBE_02 from "../assets/01_SONENSTUBE_02.jpg"
+import SONENSTUBE_03 from "../assets/01_SONENSTUBE_03.jpg"
+import SONENSTUBE_04 from "../assets/01_SONENSTUBE_04.jpg"
+import SONENSTUBE_05 from "../assets/01_SONENSTUBE_05.jpg"
+import SONENSTUBE_06 from "../assets/01_SONENSTUBE_06.jpg"
+import SONENSTUBE_07 from "../assets/01_SONENSTUBE_07.jpg"
+import SONENSTUBE_08 from "../assets/01_SONENSTUBE_08.jpg"
+import SONENSTUBE_09 from "../assets/01_SONENSTUBE_09.jpg"
+import SONENSTUBE_10 from "../assets/01_SONENSTUBE_10.jpg"
+
+import MORCOTE_01 from "../assets/03_MORCOTE_01.jpg"
+import MORCOTE_02 from "../assets/03_MORCOTE_02.jpg"
+import MORCOTE_03 from "../assets/03_MORCOTE_03.jpg"
+import MORCOTE_04 from "../assets/03_MORCOTE_04.jpg"
+
+import DARTE_01 from "../assets/04_DARTE_01.jpg"
+import DARTE_02 from "../assets/04_DARTE_02.jpg"
+import DARTE_03 from "../assets/04_DARTE_03.jpg"
+import DARTE_04 from "../assets/04_DARTE_04.jpg"
+import DARTE_05 from "../assets/04_DARTE_05.jpg"
+
+import UNIVERSO_01 from "../assets/00_UNIVERSO_01.jpg"
+import UNIVERSO_02 from "../assets/00_UNIVERSO_02.jpg"
+import UNIVERSO_03 from "../assets/00_UNIVERSO_03.jpg"
+import UNIVERSO_04 from "../assets/00_UNIVERSO_04.jpg"
+import UNIVERSO_05 from "../assets/00_UNIVERSO_05.jpg"
+import UNIVERSO_06 from "../assets/00_UNIVERSO_06.jpg"
+import UNIVERSO_07 from "../assets/00_UNIVERSO_07.jpg"
+
 export const projectList = [
 
     {
-        id: '001',
-        title: 'Project 01',
+        id: '007',
+        title: 'Sonnenstube',
         category: ['photography'],
-        date: "2024",
-        description: 'Lorem ipsum description...',
-        text: 'Lorem Ipsum text',
-        imageId: [placeholder, placeholder, placeholder, placeholder]
+        date: "2012-2020",
+        description: 'Archive photography for the independent art space Sonnenstube in Lugano',
+        text: 'I have been working for several year to document the art exibithion of Sonnenstube, an indipendent art space in Ticino',
+        imageId: [SONENSTUBE_01, SONENSTUBE_02, SONENSTUBE_03, SONENSTUBE_04, SONENSTUBE_05, SONENSTUBE_06, SONENSTUBE_07, SONENSTUBE_08, SONENSTUBE_09,SONENSTUBE_10]
     },
 
     {
-        id: '002',
-        title: 'Project 02',
-        category: ['design'],
-        date: "2024",
-        description: 'Lorem ipsum description...',
-        text: 'Lorem Ipsum text',
-        imageId: [placeholder_2, placeholder]
+        id: '008',
+        title: 'Morcote\'s Bienale',
+        category: ['graphic design'],
+        date: "2016 - 2028 – 2020",
+        description: 'Visual identity and segnalitic parcour in the beautiful village of Morcote, Ticino',
+        text: 'Creation of a visual identity for the Morcote\'s Bienale from the first to the third edition.',
+        imageId: [MORCOTE_01, MORCOTE_02, MORCOTE_03, MORCOTE_04]
     },
 
     {
-        id: '003',
-        title: 'Project 03',
-        category: ['photography', 'code'],
-        date: "2024",
-        description: 'Lorem ipsum description...',
-        text: 'Lorem Ipsum text',
-        imageId: ['exemple_01.jpg']
+        id: '09',
+        title: 'd\'Arte',
+        category: ['graphic design'],
+        date: "2012-2018",
+        description: 'Monthly contemporary art magazine',
+        text: 'Layout and content editing of a monthly print publication',
+        imageId: [DARTE_01, DARTE_02, DARTE_03, DARTE_04, DARTE_05]
     },
 
     {
-        id: '004',
-        title: 'Project 04',
-        category: ['photography', 'code'],
-        date: "2024",
-        description: 'Lorem ipsum description...',
-        text: 'Lorem Ipsum text',
-        imageId: ['exemple_01.jpg']
+        id: '10',
+        title: 'L\'Universo',
+        category: ['graphic design'],
+        date: "2012",
+        description: 'Monthly supplement of Corriere del Ticino, written by university students',
+        text: 'L\'Universo is a monthly publication supplement of Corriere del Ticino. I have created a new layout of a 24 page format ',
+        imageId: [UNIVERSO_01, UNIVERSO_02, UNIVERSO_03, UNIVERSO_04, UNIVERSO_05, UNIVERSO_06, UNIVERSO_07]
     },
-
-    {
-        id: '005',
-        title: 'Project 05',
-        category: ['design'],
-        date: "2024",
-        description: 'Lorem ipsum description...',
-        text: 'Lorem Ipsum text',
-        imageId: ['exemple_01.jpg']
-    },
-
-    {
-        id: '006',
-        title: 'Project 06',
-        category: ['photography'],
-        date: "2024",
-        description: 'Lorem ipsum description...',
-        text: 'Lorem Ipsum text',
-        imageId: ['exemple_01.jpg']
-    },
-
 
 ];
