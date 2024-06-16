c6678ae54919ab4b47ea875e349808dbed289a35
murieru <murielhediger@gmail.com>
2024-06-16 10:20:12 +0200

    mobile responsive query added

--- src/comp/Carousel.js
+++ src/comp/Carousel.js
@@ -1,20 +1,17 @@
 import { useState } from 'react'
 import '../styles/Carousel.css'
 import arrow from '../assets/arrow-right.svg'
+import { useMediaQuery } from 'react-responsive';
 
 function Carousel ({id, title, category, date, description, text, imageId}){
 
     const [currentIndex, setCurrentIndex] = useState(0);
+    const isMobile = useMediaQuery({ query: '(max-width: 480px)' });
 
     const nextSlide = (e) => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % imageId.length);
         e.stopPropagation();
     };
-
-    const focusItem = (e) => {
-        console.log('hello')
-        e.stopPropagation();
-    }
 
     return (
 
@@ -25,8 +22,14 @@ function Carousel ({id, title, category, date, description, text, imageId}){
 
             <div className='carousel-content'  style={{ transform: `translateX(-${currentIndex * (100/imageId.length)}%)` }}>
 
-                {imageId.map((image, index) => (
-                    <img alt="" key={image} src={image} className={`carousel-item ${index === currentIndex ? 'active' : ''}`} onClick={focusItem}/>
+                {imageId.map((image, index) => ( 
+
+                isMobile ? (
+                    <div key={image} style={{ backgroundImage: `url(${image})` }} className={`carousel-item ${index === currentIndex ? 'active' : ''}`} onClick={nextSlide}></div>
+                    )
+                    :(
+                    <img alt="" key={image} src={image} className={`carousel-item ${index === currentIndex ? 'active' : ''}`} onClick={nextSlide}/>
+                    )
                 )
                 )}
 
@@ -39,3 +42,25 @@ function Carousel ({id, title, category, date, description, text, imageId}){
 }
 
 export default Carousel
+
+
+/*
+
+CAROUSEL QUI FONCTIONNE
+
+
+        <div className='project-carousel'>
+
+            <span className='left-btn' onClick={nextSlide}><img alt="" src={arrow} /></span>
+
+            <div className='carousel-content'  style={{ transform: `translateX(-${currentIndex * (100/imageId.length)}%)` }}>
+
+                {imageId.map((image, index) => ( 
+                    <img alt="" key={image} src={image} className={`carousel-item ${index === currentIndex ? 'active' : ''}`} onClick={nextSlide}/>
+                )
+                )}
+
+            </div>
+
+        </div>
+*/
