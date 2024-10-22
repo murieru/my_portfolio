f0fd6361be351fed301a67ececddd0320b27129d
murieru <murielhediger@gmail.com>
2024-10-22 18:18:55 +0200

    bug fixed

--- src/comp/Projects.js
+++ src/comp/Projects.js
@@ -1,6 +1,8 @@
 import '../styles/Projects.css'
+/*
 import {projectList} from '../data/projectList.js'
 import Project from './Project.js'
+*/
 
 function Projects({currentCategory, setCategory, isOpen}){        
 
@@ -9,7 +11,7 @@ function Projects({currentCategory, setCategory, isOpen}){
 
         <div id='projects'>
         <p>
-        <a href="https://www.figma.com/proto/iddGniBHX0LrV19mGXHAXR/UX-%2F-UI-Portfolio?page-id=3%3A40&node-id=3-77&node-type=frame&viewport=1007%2C707%2C0.1&t=lrvG1vni8RUebutc-1&scaling=contain&content-scaling=fixed&starting-point-node-id=3%3A77" target="_blank">To visit my actual portfolio, please folow this link</a>
+        <a href="https://www.figma.com/proto/iddGniBHX0LrV19mGXHAXR/UX-%2F-UI-Portfolio?page-id=3%3A40&node-id=3-77&node-type=frame&viewport=1007%2C707%2C0.1&t=lrvG1vni8RUebutc-1&scaling=contain&content-scaling=fixed&starting-point-node-id=3%3A77">To visit my actual portfolio, please folow this link</a>
         </p>
         </div>
         
